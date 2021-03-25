74cde0b6af41ba460a1bfd6fdccdf6b1c34d3c9f
grhgrmgrhrm <grhgrmgrhrm.gmail.com>
2021-03-25 21:55:16 +0300

    fix: add solution for carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,14 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY = 15;
-const HALF_LIFE_PERIOD = 5730;
+const MODERN_ACTIVITY= 15;
+const HALF_LIFE_PERIOD= 5730;
+const k = Math.LN2 / HALF_LIFE_PERIOD;
 
 module.exports = function dateSample(sampleActivity) {
-    if (typeof sampleActivity !== Number) {
+    if ((typeof sampleActivity !== "string" ) || (isNaN(sampleActivity)) ) {
         return false;
+    } else {
+        let answer = Math.log( MODERN_ACTIVITY / sampleActivity ) / k;
+
+        if ((Math.ceil(answer) !== Infinity) && (Math.ceil(answer) > 0)) {
+            return Math.ceil(answer);
+        }
+            return false;
     }
-    const k = 0.693 / HALF_LIFE_PERIOD;
-    let answer = Math.log(sampleActivity / MODERN_ACTIVITY) / k;
-    if (answer < 0) answer *= -1;
-    return Math.ceil(answer);
 };
+
